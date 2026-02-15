d59adaaf8841f0a40d36b2b17df7161dcc438708
Lavanya Paleti <srilavanya.paleti@gmail.com>
2026-02-14 23:37:08 -0700

    Fix Pages routing (base + trailingSlash)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://srilavanyapaleti.github.io",
-  base: "/lavanyapaleti.github.io/",
+  base: "/lavanyapaleti.github.io",
+  trailingSlash: "always",
 });
